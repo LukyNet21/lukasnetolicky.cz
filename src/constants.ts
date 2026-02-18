da9746c869709e8873a0ce35ce797332ec4e6040
lukynet <98685127+LukyNet21@users.noreply.github.com>
2026-02-18 20:54:21 +0100

    content update and added technologies

--- src/constants.ts
+++ src/constants.ts
@@ -11,9 +11,11 @@ import tailwindIconWhite from "./assets/icons/tailwindcss-white.svg"
 import goIcon from "./assets/icons/go.svg"
 import postgreIcon from "./assets/icons/postgresql.svg"
 import nextjsIcon from "./assets/icons/nextjs.svg"
+import honoIcon from "./assets/icons/hono.svg"
 
 import gitIcon from "./assets/icons/git.svg"
 import dockerIcon from "./assets/icons/docker.svg"
+import cloudflareIcon from "./assets/icons/cloudflare.svg"
 
 export const techCategories = [
   { key: "frontend", title: "Frontend", title_cs: "Frontend" },
@@ -23,20 +25,22 @@ export const techCategories = [
 
 export const technologies = [
   // Frontend
-  { name: "React", url: "https://react.dev/" ,category: "frontend", icon: reactIcon },
-  { name: "Astro", url: "https://astro.build/", category: "frontend", icon: astroIcon, lightModeIcon: astroIconDark },
+  { name: "React", url: "https://react.dev/", category: "frontend", icon: reactIcon, homePage: true, featured: true },
+  { name: "Astro", url: "https://astro.build/", category: "frontend", icon: astroIcon, lightModeIcon: astroIconDark, homePage: true },
   { name: "TypeScript", url: "https://www.typescriptlang.org/", category: "frontend", icon: tsIcon },
   { name: "JavaScript", category: "frontend", icon: jsIcon },
-  { name: "HTML", category: "frontend", icon: htmlIcon },
+  { name: "HTML", category: "frontend", icon: htmlIcon, homePage: true },
   { name: "CSS", category: "frontend", icon: cssIcon },
-  { name: "Tailwind CSS", url: "https://tailwindcss.com/", category: "frontend", icon: tailwindIconWhite, lightModeIcon: tailwindIcon },
+  { name: "Tailwind CSS", url: "https://tailwindcss.com/", category: "frontend", icon: tailwindIconWhite, lightModeIcon: tailwindIcon, homePage: true },
 
   // Backend
-  { name: "Go", url: "https://go.dev/", category: "backend", icon: goIcon },
-  { name: "PostgreSQL", url: "https://www.postgresql.org/", category: "backend", icon: postgreIcon },
+  { name: "Go", url: "https://go.dev/", category: "backend", icon: goIcon, homePage: true, featured: true },
+  { name: "PostgreSQL", url: "https://www.postgresql.org/", category: "backend", icon: postgreIcon, homePage: true },
+  { name: "Hono", url: "https://hono.dev/", category: "backend", icon: honoIcon, homePage: true },
   { name: "Next.js", url: "https://nextjs.org/", category: "backend", icon: nextjsIcon },
 
   // Tools
-  { name: "Git", url: "https://git-scm.com/", category: "tools", icon: gitIcon },
-  { name: "Docker", url: "https://www.docker.com/", category: "tools", icon: dockerIcon },
+  { name: "Git", url: "https://git-scm.com/", category: "tools", icon: gitIcon, homePage: true, featured: true },
+  { name: "Docker", url: "https://www.docker.com/", category: "tools", icon: dockerIcon, homePage: true, featured: true },
+  { name: "Cloudflare", url: "https://www.cloudflare.com/", category: "tools", icon: cloudflareIcon, homePage: true },
 ];
